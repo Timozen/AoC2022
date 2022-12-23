4dd118513dfdf47b511908f04f68e9cea7c56e2a
Tim Büchner <tim.buechner@uni-jena.de>
2022-12-23 09:15:27 +0100

    Make the code more readable
    Introduce Pos namedTuple for positions of the elves

--- 23/23.py
+++ 23/23.py
@@ -1,17 +1,18 @@
 import pathlib
 import collections
 
-DIR = collections.namedtuple('DIR', ['x', 'y'])
+Dir = collections.namedtuple('Dir', ['x', 'y'])
+Pos = collections.namedtuple('Pos', ['x', 'y'])
 
 DIRS = {
-    "N":  DIR( 0, -1),
-    "S":  DIR( 0,  1),
-    "E":  DIR( 1,  0),
-    "W":  DIR(-1,  0),
-    "NE": DIR( 1, -1),
-    "NW": DIR(-1, -1),
-    "SE": DIR( 1,  1),
-    "SW": DIR(-1,  1),
+    "N":  Dir( 0, -1),
+    "S":  Dir( 0,  1),
+    "E":  Dir( 1,  0),
+    "W":  Dir(-1,  0),
+    "NE": Dir( 1, -1),
+    "NW": Dir(-1, -1),
+    "SE": Dir( 1,  1),
+    "SW": Dir(-1,  1),
 }
 DIRS_ALL = {
     "A" : list(DIRS.keys()),
@@ -21,15 +22,15 @@ DIRS_ALL = {
     "W" : ["W", "NW", "SW"],
 }
 
-def field_size(elves: set[tuple[int, int]]) -> tuple[int, int, int, int]:
+def field_size(elves: set[Pos]) -> tuple[int, int, int, int]:
     return (
-        min(elves, key=lambda x: x[0])[0],
-        min(elves, key=lambda x: x[1])[1],
-        max(elves, key=lambda x: x[0])[0],
-        max(elves, key=lambda x: x[1])[1]
+        min(elves, key=lambda p: p.x)[0],
+        min(elves, key=lambda p: p.y)[1],
+        max(elves, key=lambda p: p.x)[0],
+        max(elves, key=lambda p: p.y)[1]
     )
 
-def print_elves_location(elves: set[tuple[int,int]]) -> None:
+def print_elves_location(elves: set[Pos]) -> None:
     # the locations are in an arbitrary range, so we need to find the max x and y, and the min x and y
     min_x, min_y, max_x, max_y = field_size(elves)
     # move the min_x and min_y to 0,0
@@ -39,29 +40,28 @@ def print_elves_location(elves: set[tuple[int,int]]) -> None:
     max_x, max_y = max_x + offset_x, max_y + offset_y
     
     grid = [['.' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
-    for x, y in elves:
-        grid[y + offset_y][x + offset_x] = '#'
+    for p in elves:
+        grid[p.y + offset_y][p.x + offset_x] = '#'
         
     for row in grid:
         print(''.join(row))
 
-def has_adjacent_elves(elves: set[tuple[int,int]], elve: tuple[int,int], d:str="A") -> bool:
+def has_adjacent_elves(elves: set[Pos], elve: Pos, d:str="A") -> bool:
     for key in DIRS_ALL[d]:
         dir = DIRS[key]
-        if (elve[0] + dir.x, elve[1] + dir.y) in elves:
+        if Pos(elve.x + dir.x, elve.y + dir.y) in elves:
             return True
     return False
 
-def elve_move(elve: tuple[int,int], d: str) -> tuple[int,int]:
-    dir = DIRS[d]
-    return (elve[0] + dir.x, elve[1] + dir.y)
+def elve_move(elve: Pos, d: str) -> Pos:
+    return Pos(elve.x + DIRS[d].x, elve.y + DIRS[d].y)
 
-def get_or_create(d: dict, key: tuple[int, int], default: list) -> list:
+def get_or_create(d: dict[Pos, list[Pos]], key: Pos, default: list) -> list:
     if key not in d:
         d[key] = default
     return d[key]
 
-def diffuse(elves: set[tuple[int,int]], move_dirs: collections.deque[str]) -> bool:
+def diffuse(elves: set[Pos], move_dirs: collections.deque[str]) -> bool:
     new_location = {}
     
     any_moved = False
@@ -97,7 +97,7 @@ def diffuse(elves: set[tuple[int,int]], move_dirs: collections.deque[str]) -> bo
 lines = pathlib.Path('input.txt').read_text().splitlines()
 
 # # are the elves and . is empty space
-elves = set((x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == '#')
+elves = set(Pos(x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == '#')
 move_dirs = collections.deque(["N", "S", "W", "E"])
 
 for rdx in range(10):
@@ -114,7 +114,7 @@ print("Task 1", f_size - len(elves))
 # Task 2
 # continue until no elf moves
 # # are the elves and . is empty space
-elves = set((x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == '#')
+elves = set(Pos(x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == '#')
 move_dirs = collections.deque(["N", "S", "W", "E"])
 
 round_cnt = 0
